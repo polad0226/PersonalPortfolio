40254c5b09500f9ae6c567f5a014819fb188b1e3
polad0226 <polad.yunisov.001@gmail.com>
2023-09-10 20:48:56 -0500

    e

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import Blog from '../../components/container/blog';
 import Contacts from '../../components/container/contacts';
 import Landing from '../../components/container/landing';
 import Navbar from '../../components/container/navbar';
