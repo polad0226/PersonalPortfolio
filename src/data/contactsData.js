c835cb7c19e93b7aa8a77b305c4b6883170866e4
polad0226 <polad.yunisov.001@gmail.com>
2023-09-11 13:32:33 -0500

    address change

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
   email: 'polad.yunisov.001@gmail.com',
   phone: '2058763431',
-  address: '2938 Columbiana CT APT C',
+  address: 'Birmingham,Alabama, USA',
   github: 'https://github.com/polad0226',
   facebook: 'https://www.facebook.com/',
   linkedIn: 'https://www.linkedin.com/in/polad-yunisov/',
