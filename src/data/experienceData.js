1f9c54b658e721cccb4780063603c13949bcc98b
polad0226 <polad.yunisov.001@gmail.com>
2024-07-28 15:31:40 -0400

    update job exp

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,22 +1,30 @@
 export const experienceData = [
   {
     id: 1,
-    company: 'XpertDox',
-    jobtitle: 'Software Engineer',
+    company: 'Exotec',
+    jobtitle: 'Software Integration Engineer',
     startYear: 'Jan 2023',
     endYear: 'Present',
   },
+
   {
     id: 2,
+    company: 'XpertDox',
+    jobtitle: 'Full Stack Developer',
+    startYear: 'Jan 2023',
+    endYear: 'Jan 2024',
+  },
+  {
+    id: 3,
     company: 'Solidshape',
     jobtitle: 'FrontEnd Developer Intern',
     startYear: 'Jun 2022',
     endYear: 'Aug 2022',
   },
   {
-    id: 3,
+    id: 4,
     company: 'Wireless World',
-    jobtitle: 'Full Stack Developer',
+    jobtitle: 'Full Stack Developer Intern',
     startYear: ' Jun 2021',
     endYear: 'Nov 2021',
   },
