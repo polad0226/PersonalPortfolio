488e57ad2cb3820d6f2e3293238a2c5e563fd86e
polad0226 <polad.yunisov.001@gmail.com>
2024-07-28 15:42:10 -0400

    info general

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,5 +3,5 @@ export const aboutData = {
   description1:
     "My name is Polad Yunisov. I'm a Software Engineer based in the United States",
   description2:
-    'As a computer science graduate student, I offer a strong and continually expanding understanding of programming and software engineering concepts.Particular strengths in full-stack development, front-end web development, and back- end development with an optimistic outlook at solving problems with high-tech solutions.',
+    'A Software Developer with 3 years of experience, I excel at delivering fast, effective solutions.My proficiency in JavaScript and Python, combined with a solid foundation in data structures and algorithms, drives my commitment to sustainable and maintainable code throughObject-Oriented Programming and SOLID principles. My expertise also includes website creation and REST API backend development, enabling the creation of robust applications and services.',
 };
